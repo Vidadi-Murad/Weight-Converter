1f696a151c3a67648ef1a42a62659d2af90ff851
VidadiMurad <vidad0murad@gmail.com>
2026-04-17 02:08:17 +0400

    Added Program Title

--- Enter/src/App.java
+++ Enter/src/App.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        // Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Weight Conversion Program");
+
+        
+
+
+        scanner.close();
     }
 }
