ddf997031bb4ff126c89b39bea3d164f2d5bcd18
VidadiMurad <vidad0murad@gmail.com>
2026-04-17 02:11:22 +0400

    Added Options

--- Enter/src/App.java
+++ Enter/src/App.java
@@ -6,10 +6,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         // Scanner
+
+        System.out.println("---------------------------");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Weight Conversion Program");
 
-        
+        // Options
+        String firstOption = "1: Convert LBS to KGS";
+        String secondOption = "2: Convert KGS to LBS";
+
+        System.out.println(firstOption);
+        System.out.println(secondOption);
+
+
 
 
         scanner.close();
